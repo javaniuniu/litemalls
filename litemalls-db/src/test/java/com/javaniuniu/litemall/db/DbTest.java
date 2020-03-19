eceb3cffe82e5e19eb87ea0a4049a0a9bb36e1c9
minp <king101125s@gmail.com>
2020-03-19 21:19:43 +0800

    other config
    other config

--- litemalls-db/src/test/java/com/javaniuniu/litemall/db/DbTest.java
+++ litemalls-db/src/test/java/com/javaniuniu/litemall/db/DbTest.java
@@ -16,7 +16,9 @@ public class DbTest {
         String chars = "ttdddty";
 
         int[] c = new int[26];
-        int s = 't'-'a';
+        int s = 'z'-'A';
+        int s2 = 'a'-'a';
+        int s3 = 3/2;
         c[s] = 1;
         for(char cc : chars.toCharArray()) {
             c[(cc - 'a')] += 1;
@@ -36,6 +38,37 @@ public class DbTest {
         }
 //        return res;
 
+        System.out.println(res);
+    }
+
+
+    @Test
+    public void longestPalindrome() {
+        String s = "zeusnilemacaronimaisanitratetartinasiaminoracamelinsuez";
+        int[] c = new int[58];
+        for(char cc:s.toCharArray()) {
+            c[(int)cc-'A']+=1;
+        }
+        int res = 0;
+        boolean flg = false;
+        for(int i = 0; i<58; i++) {
+            if(c[i]%2==0&&c[i]!=0){
+                res+=c[i];
+//                flg = true;
+            }
+            if(c[i]%2==1){
+                res+=c[i]-1;
+                flg = true;
+            }
+        }
+        if (flg) {
+//            return res+1;
+            System.out.println(res+1);
+        }else {
+//            return res;
+            System.out.println(res);
+        }
+//        return res;
         System.out.println(res);
     }
 
