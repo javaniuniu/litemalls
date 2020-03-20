981c74cc3e58d546bf3555875f261e2e6b86ec06
minp <king101125s@gmail.com>
2020-03-20 22:00:42 +0800

    Create CharUtil.java
    创建数字随机数和字母随机数

---
+++ litemalls-core/src/main/java/com/javaniuniu/litemall/core/util/CharUtil.java
@@ -0,0 +1,35 @@
+package com.javaniuniu.litemall.core.util;
+
+import java.util.Random;
+
+/**
+ * 随机数
+ *
+ * @Author: java牛牛
+ * @Web: http://javaniuniu.com
+ * @GitHub https://github.com/javaniuniu
+ * @Date: 2020/3/20 9:56 PM
+ */
+public class CharUtil {
+    public static String getRandomString(Integer num) {
+        String base = "abcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < num; i++) {
+            int number = random.nextInt(base.length());
+            sb.append(base.charAt(number));
+        }
+        return sb.toString();
+    }
+
+    public static String getRandomNum(Integer num) {
+        String base = "0123456789";
+        Random random = new Random();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < num; i++) {
+            int number = random.nextInt(base.length());
+            sb.append(base.charAt(number));
+        }
+        return sb.toString();
+    }
+}
