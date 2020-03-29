2020e613000bbd04b54743060e3b4c86a0c4d813
minp <king101125s@gmail.com>
2020-03-29 21:03:18 +0800

    工具类
    Bean 工具类
    时间格式化工具类

---
+++ litemalls-core/src/main/java/com/javaniuniu/litemall/core/util/BeanUtil.java
@@ -0,0 +1,30 @@
+package com.javaniuniu.litemall.core.util;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * Bean 工具类
+ *
+ * @Author: java牛牛
+ * @Web: http://javaniuniu.com
+ * @GitHub https://github.com/javaniuniu
+ * @Date: 2020/3/29 8:56 PM
+ */
+public class BeanUtil implements ApplicationContextAware {
+    protected static ApplicationContext context;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        context = applicationContext;
+    }
+
+    public static Object getBean(String name) {
+        return context.getBean(name);
+    }
+
+    public static <T> T getBean(Class<T> c) {
+        return context.getBean(c);
+    }
+}
---
+++ litemalls-core/src/main/java/com/javaniuniu/litemall/core/util/DateTimeUtil.java
@@ -0,0 +1,33 @@
+package com.javaniuniu.litemall.core.util;
+
+/**
+ * 日期格式化工具类
+ *
+ * @Author: java牛牛
+ * @Web: http://javaniuniu.com
+ * @GitHub https://github.com/javaniuniu
+ * @Date: 2020/3/29 8:59 PM
+ */
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * 日期格式化工具类
+ */
+public class DateTimeUtil {
+
+    /**
+     * 格式 yyyy年MM月dd日 HH:mm:ss
+     *
+     * @param dateTime
+     * @return
+     */
+    public static String getDateTimeDisplayString(LocalDateTime dateTime) {
+        DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("yyyy年MM月dd日 HH:mm:ss");
+        String strDate2 = dtf2.format(dateTime);
+        return strDate2;
+    }
+
+}
