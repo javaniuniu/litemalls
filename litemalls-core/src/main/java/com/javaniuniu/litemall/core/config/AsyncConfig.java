201f3c79ec2efa447dd64b4c6c1a25cd94014fa2
minp <king101125s@gmail.com>
2020-03-30 13:39:56 +0800

    异步配置
    异步配置

---
+++ litemalls-core/src/main/java/com/javaniuniu/litemall/core/config/AsyncConfig.java
@@ -0,0 +1,16 @@
+package com.javaniuniu.litemall.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+/**
+ * 异步配置
+ * @Author: java牛牛
+ * @Web: http://javaniuniu.com
+ * @GitHub https://github.com/javaniuniu
+ * @Date: 2020/3/30 12:41 PM
+ */
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
