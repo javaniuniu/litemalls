9d58de6549964da8f6303a4d6bab2a749ee3a629
minp <king101125s@gmail.com>
2020-03-30 13:39:48 +0800

    跨域配置
    跨域配置

---
+++ litemalls-core/src/main/java/com/javaniuniu/litemall/core/config/CorsConfig.java
@@ -0,0 +1,37 @@
+package com.javaniuniu.litemall.core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * 跨域配置
+ *
+ * @Author: java牛牛
+ * @Web: http://javaniuniu.com
+ * @GitHub https://github.com/javaniuniu
+ * @Date: 2020/3/30 12:46 PM
+ */
+@Configuration
+public class CorsConfig {
+    // 当前跨域请求最大有效时长。这里默认30天
+    private long maxAge = 30 * 24 * 60 * 60;
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*"); // 1 设置访问源地址
+        corsConfiguration.addAllowedHeader("*"); // 2 设置访问源请求头
+        corsConfiguration.addAllowedMethod("*"); // 3 设置访问源请求方法
+        corsConfiguration.setMaxAge(maxAge);
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig()); // 4 对接口配置跨域设置
+        return new CorsFilter(source);
+    }
+}
