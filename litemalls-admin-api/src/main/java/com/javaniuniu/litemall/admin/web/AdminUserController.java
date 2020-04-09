303521006a1dbc52ffec500c02f95b7952051155
minp <king101125s@gmail.com>
2020-04-09 17:59:58 +0800

    Controller 类
    Controller 类

---
+++ litemalls-admin-api/src/main/java/com/javaniuniu/litemall/admin/web/AdminUserController.java
@@ -0,0 +1,41 @@
+package com.javaniuniu.litemall.admin.web;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import com.javaniuniu.litemall.admin.annotation.RequiresPermissionsDesc;
+import com.javaniuniu.litemall.core.util.ResponseUtil;
+import com.javaniuniu.litemall.core.validator.Order;
+import com.javaniuniu.litemall.core.validator.Sort;
+import com.javaniuniu.litemall.db.domain.LitemallUser;
+import com.javaniuniu.litemall.db.service.LitemallUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/user")
+@Validated
+public class AdminUserController {
+    private final Log logger = LogFactory.getLog(AdminUserController.class);
+
+    @Autowired
+    private LitemallUserService userService;
+
+    @RequiresPermissions("admin:user:list")
+    @RequiresPermissionsDesc(menu = {"用户管理", "会员管理"}, button = "查询")
+    @GetMapping("/list")
+    public Object list(String username, String mobile,
+                       @RequestParam(defaultValue = "1") Integer page,
+                       @RequestParam(defaultValue = "10") Integer limit,
+                       @Sort @RequestParam(defaultValue = "add_time") String sort,
+                       @Order @RequestParam(defaultValue = "desc") String order) {
+        List<LitemallUser> userList = userService.querySelective(username, mobile, page, limit, sort, order);
+        return ResponseUtil.okList(userList);
+    }
+}
