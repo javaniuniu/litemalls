3b364a55f4f69226680c0f06e26bbf8d77d0a350
minp <king101125s@gmail.com>
2020-04-09 16:58:40 +0800

    通过注解方式 说明请求权限
    通过注解方式 说明请求权限

---
+++ litemalls-admin-api/src/main/java/com/javaniuniu/litemall/admin/annotation/RequiresPermissionsDesc.java
@@ -0,0 +1,21 @@
+package com.javaniuniu.litemall.admin.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 请求权限说明
+ * @Author: java牛牛
+ * @Web: http://javaniuniu.com
+ * @GitHub https://github.com/javaniuniu
+ * @Date: 2020/4/9 4:49 PM
+ */
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RequiresPermissionsDesc {
+    String[] menu();
+
+    String button();
+}
