add4f43c1381b7da5437757bd4c21dac49e47140
minp <king101125s@gmail.com>
2020-04-09 17:01:36 +0800

    配置
    1.swagger在线文档配置
    2.shiro 配置
    3.登陆异常普获

---
+++ litemalls-admin-api/src/main/java/com/javaniuniu/litemall/admin/config/AdminSwagger2Configuration.java
@@ -0,0 +1,45 @@
+package com.javaniuniu.litemall.admin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * swagger在线文档配置<br>
+ * 项目启动后可通过地址：http://host:ip/swagger-ui.html 查看在线文档
+ * @Author: java牛牛
+ * @Web: http://javaniuniu.com
+ * @GitHub https://github.com/javaniuniu
+ * @Date: 2020/4/9 4:50 PM
+ */
+@Configuration
+@EnableSwagger2
+public class AdminSwagger2Configuration {
+    @Bean
+    public Docket adminDocket() {
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("admin")
+                .apiInfo(adminApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.javaniuniu.litemall.admin.web"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo adminApiInfo() {
+        return new ApiInfoBuilder()
+                .title("litemall-admin API")
+                .description("litemall管理后台API")
+                .termsOfServiceUrl("https://github.com/linlinjava/litemall")
+                .contact("https://github.com/linlinjava/litemall")
+                .version("1.1")
+                .build();
+    }
+}
