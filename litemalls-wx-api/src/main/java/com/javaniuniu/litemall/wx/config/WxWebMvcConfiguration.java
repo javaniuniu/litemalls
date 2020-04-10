71c18959e453221330ca9f4b2055ee5c60b9bc4d
minp <king101125s@gmail.com>
2020-04-10 16:54:13 +0800

    自定义解析器
    自定义解析器

---
+++ litemalls-wx-api/src/main/java/com/javaniuniu/litemall/wx/config/WxWebMvcConfiguration.java
@@ -0,0 +1,22 @@
+package com.javaniuniu.litemall.wx.config;
+
+import com.javaniuniu.litemall.wx.annotation.support.LoginUserHandlerMethodArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * @Author: java牛牛
+ * @Web: http://javaniuniu.com
+ * @GitHub https://github.com/javaniuniu
+ * @Date: 2020/4/10 4:39 PM
+ */
+@Configuration
+public class WxWebMvcConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new LoginUserHandlerMethodArgumentResolver());
+    }
+}
